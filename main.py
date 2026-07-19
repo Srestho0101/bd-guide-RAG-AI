9fc563ffd8645e04d8b42e263266919bacf588d8
Srestho <srestho1298@gmail.com>
2026-07-19 17:27:35 +0600

    Update CORS settings to allow all origins

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 # Enable CORS so your WordPress server/frontend can communicate with it
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["amarbangladesh.com", "srestho0101.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
